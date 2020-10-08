8d9c3d643547fae6d1b01a78995c8297438e7a28
DeaVenditama <deavenditama@gmail.com>
2020-10-08 10:03:48 +0700

    first push

---
+++ src/HelloVue.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Hello World</h1>
+    <h4>Project Vue Pertama Saya</h4>
+    <h4>Nama Saya {{ nama }}</h4>
+    <h4>Umur Saya {{ umur }} tahun</h4>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            nama : "Dea Venditama",
+            umur : 17   
+        }
+    },
+    mounted(){
+        this.umur = this.umur + 3;        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Model.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Penggunaan V Model</h1>   
+    <h4>{{ messages }}</h4>
+    <input v-model="messages" />    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            messages : ''
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>
